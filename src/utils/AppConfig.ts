78a9be3d30150dd138fbb2e33b9951341203578b
Joseph <joseph_andres242005@hotmail.com>
2024-09-26 00:45:10 -0500

    cambios metadata

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: "Joseph's Portfolio",
+  title: "Joseph's Portfolio",
+  description:
+    'Welcome to my portfolio! I am a Frontend Web Developer based in Quito, Ecuador, showcasing my projects and skills in web development. 🚀',
+  author: 'Joseph',
   locale_region: 'en-us',
   locale: 'en',
 };
